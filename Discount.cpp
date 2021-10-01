0a85bc252e6f4ae496e66c62f5e2cc8982f6c76e
Sugandha <72730060+Sugandha-kadp@users.noreply.github.com>
2021-10-02 00:38:38 +0530

    Create Discount.cpp

---
+++ Discount.cpp
@@ -0,0 +1,32 @@
+#include <stdio>
+using namespace stdd;
+
+int main()
+{
+
+ float pay,discount;
+
+ cout << "Enter Payment amount : "
+ cout >> pay;
+
+    if (p > 10000)
+    {
+      discount=pay*25/100
+    }
+
+    else if (pay > 5000)
+    {
+      discount=pay*15/100;
+    }
+
+    else if (pay > 3000)
+    {
+      discount=pay*10/100;
+    }
+
+    else
+    discount= 0;
+
+ cout << "Discount  is -> " << pay << endl;
+
+}
