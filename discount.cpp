1f50d958b9394fd318d185a4659a5890a8224b79
Satyam mahajan <54473091+satyam-mahajan@users.noreply.github.com>
2021-10-05 01:43:40 +0530

    Create discount.cpp

---
+++ discount.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+ float pay,discount;
+
+ cout << "Enter Payment amount : "
+ cout >> pay;
+
+    if (p > 10000)
+    {
+      discount = (pay*25)/100
+    }
+
+    else if (pay > 5000)
+    {
+      discount = (pay*15)/100;
+    }
+
+    else if (pay > 3000)
+    {
+      discount = (pay*10)/100;
+    }
+
+    else
+    {
+      discount = 0;
+    }
+   cout << "Discount  is -> " << discount << endl;
+   return 0;
+
+}
